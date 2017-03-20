95994ef7837c98e05739a8b5ecdf3e83070b7ad4
ed alegrid <ealegrid@gmail.com>
2017-03-20 18:59:18 -0400

    fixed tcp control valid data

--- tcp/server.js
+++ tcp/server.js
@@ -2,8 +2,9 @@
 'use strict'
 const net = require('net');
 const IOControlModule = require('../lib/ipcheck.js');
-const WebControl = require('../lib/WebControl.js');
+const Web = require('../lib/WebControl.js');
 const opCode = require('../lib/WebControl.js').opCode;
+const gpCode = require('../lib/WebControl.js').gpCode;
 
 var remoteIP; 
 
@@ -15,21 +16,33 @@ exports.start = (io, port, cb) => {
   server.on('data', (payload) => {
   console.log('TcpServer:', payload.toString());
   var _cd = payload.toString();
+
     if (server.remoteAddress !== '::ffff:127.0.0.1'){
 
-    exports.server = server;
-    remoteIP = IOControlModule.sanitizedIP(server);
+      exports.server = server;
+      remoteIP = IOControlModule.sanitizedIP(server);
 
-    if(_cd === opCode.on || _cd === opCode.off){
-    exports.remoteIP = IOControlModule.sync(_cd, remoteIP);
-    return;
-    }
-  
-    exports.remoteIP;
-
-    console.log('\n**** Tcp Control ****');
-    console.log('client data: ', _cd);
-    WebControl.operation(_cd);
+      if(_cd === opCode.on || _cd === opCode.off){
+        exports.remoteIP = IOControlModule.sync(_cd, remoteIP);
+        return;
+      }
+      exports.remoteIP;
+      console.log('\n**** Tcp Control ****');
+      //console.log('remote client data (port: 51111): ', _cd);
+      if(_cd === opCode.state) {
+        Web.Control(_cd);
+      }
+      else if(gpCode){
+        for(let x in gpCode.out.on){
+          if(gpCode.out.on[x] === _cd || gpCode.out.off[x] === _cd){
+            return Web.Control(_cd);
+          }
+        }
+      }
+      else{
+         console.log('gpCode is null, cannot control GPIO outputs');
+         return;   
+      }
     }
     else {
       io.emit('browserEvent', _cd); 
