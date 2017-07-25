ade2afda80d4f08aa7aee1020af1b6b3cdfb699e
ed alegrid <ealegrid@gmail.com>
2017-07-25 09:32:30 -0400

    edit comments

--- routes/device.js
+++ routes/device.js
@@ -1,5 +1,6 @@
 /* Author: Ed Alegrid 3/13/2017 */
-'use strict'
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var Web = require('../lib/WebControl.js');
@@ -16,18 +17,19 @@ console.log('\n**** HTTP (req, res) ****');
 console.log('request data: ', item);
 
 /* 
-*  Using a single control function to get the current pin state,
-*  control a GPIO pin and update the browser for ON/OFF state condition. 
-*/ 
+ *  Using a single control function to get the current pin state,
+ *  control a GPIO pin and update the browser for ON/OFF state condition. 
+ */ 
 Web.Control(_cd);
 
 result = [_cd];
 console.log('response data:', result);
 
 setTimeout( function() {
-var data = JSON.stringify(result);
-res.json({item: data.toString()});
+  var data = JSON.stringify(result);
+  res.json({item: data.toString()});
 }, 25 ); 
+
 });
 module.exports = r;   
 
