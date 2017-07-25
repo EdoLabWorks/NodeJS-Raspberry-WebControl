a9382738a6bff3dfd6d88540d274f19936c4ab40
ed alegrid <ealegrid@gmail.com>
2017-07-25 09:30:00 -0400

    refactor

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const TcpServer = require('./tcp/server.js');
 const gpio = require('./lib/WebControl.js');
 
 /*
-*  Provide the GPIO pin no. of your input and output object (max. of 6 inputs and 6 outputs).  
-*  (replace the default setup below using the actual pins you will use)  
-*/
+ *  Provide the GPIO pin no. of your input and output object (max. of 6 inputs and 6 outputs).  
+ *  (replace the default setup below using the actual pins you will use)  
+ */
 gpio.setInput(11,13,15,19);
 gpio.setOutput(33,35,37,36,38,40); 
 
 var ip;
+
 function webStart(){
 /* start tcp server with socket.io for browser button updates */
 TcpServer.start(io, 5555, ()=>{ //using port 5555
